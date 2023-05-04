8dd9ea8ec48c3d798ff8622af3604003a9a8ce7f
mdshahjalal2 <muhommodshahjalal@gmail.com>
2023-05-04 12:33:50 +0600

    d

--- src/null_unknown_never.ts
+++ src/null_unknown_never.ts
@@ -1,4 +1,5 @@
 // !n! nun => nul un ne => null unknown never 
+// ! nev=> never void => un in =>  unknown implicit any
 // ! null 
 const searchName = (value: string | null):void => {
     if (value === null) {
@@ -31,7 +32,7 @@ getMyCarSpeed(10);
 getMyCarSpeed("10 kmh^-1"); // 10 kmh-1
 getMyCarSpeed(true);
 
-//  !n! never // the function never return anything 
+//  !n! never // the function never return anything // can't we use void // why never 
 function throwError(message: string): never {
     throw new Error(message);
 }
