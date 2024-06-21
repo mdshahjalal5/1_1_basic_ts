7425db8729352e05b37f46a91f926438f19695ce
mdshahjalal5 <muhommodshahjalal@gmail.com>
2024-06-21 16:26:33 +0600

    done

--- src/spread_rest_default_param_destructuring.ts
+++ src/spread_rest_default_param_destructuring.ts
@@ -1,21 +1,19 @@
 //   !n! serd => spe res def ring => spread => rest => default parameter => destrucuring => ]
 
-// !n! ts rest 
-// ! rest parameter 
-const cube = (...nums:number[]) => nums.map(n => n*n*n)
-const cubeRes = cube(1, 2, 3, 4, 5, 6, 7, 8)
-console.log(cubeRes, '"cube"')
+// ! rest parameter : Rest Parameter: (...nums: number[]) allows the function to accept an indefinite number of arguments, which are gathered into an array named nums
+const cube = (...nums: number[]) => nums.map((n) => n * n * n);
+const cubeRes = cube(1, 2, 3, 4, 5, 6, 7, 8);
+console.log(cubeRes, '"cube"');
 
-// ! void only use when no return value is provided
-const voidFunction = (n:number):void => console.log(n*n)
-voidFunction(3)
+// ! void only use when a function does not  return any  value
+const voidFunction = (n: number): void => console.log(n * n);
+voidFunction(3);
 var aliasing = {
-    name: 'John',
-    age: 30
-}
+  name: "John",
+  age: 30,
+};
 
-const {age:nameAlias} = aliasing
-console.log(nameAlias, '"nameAlias"')
+const { age: nameAlias } = aliasing;
+console.log(nameAlias, '"nameAlias"');
 
 export default cube;
-
