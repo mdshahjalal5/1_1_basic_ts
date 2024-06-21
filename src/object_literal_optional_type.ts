7b012cc86e3354bbcbb600cb92d6e2b2eb290184
mdshahjalal5 <muhommodshahjalal@gmail.com>
2024-06-21 16:06:02 +0600

    done

--- src/object_literal_optional_type.ts
+++ src/object_literal_optional_type.ts
@@ -1,45 +1,42 @@
-//  !fi! 
+//  !fi!
 
 //  !n! einlim => expplicit =>implicit=> intersection => literal =>modifier =>
 
-// ! implicit type 
+// ! implicit type
 const profile = {
-    name: 'shah',
-    age: 20,
-    profession: 'dev',
-    isMarried: false,
-    phone: 33349
-}
+  name: "shah",
+  age: 20,
+  profession: "dev",
+  isMarried: false,
+  phone: 33349,
+};
 // ! profile.name = 3 ** Type 'number' is not assignable to type 'string'.
 
-// !explicit type 
+// !explicit type
 const prof2: {
-    name: string,
-    age: number,
-    profession: 'student',  // ! literal type 
-    isMarried: false,
-    wifeName?: string // !n! Intersection of an object's value types in TypeScript
+  name: string;
+  age: number;
+  profession: "student"; // ! literal type
+  isMarried: false;
+  wifeName?: string; // !n! optional type  of an object in TypeScript
 } = {
-    age: 3,
-    // ! profession:'students',  //! [ts] Type '"students"' is not assignable to type '"student"'.
-    profession: 'student',
-    isMarried: false,
-    name: 'shah'
-}
+  age: 3,
+  // ! profession:'students',  //! [ts] Type '"students"' is not assignable to type '"student"'.
+  profession: "student",
+  isMarried: false,
+  name: "shah",
+};
 
 const car: {
-    readonly type: string,  // !n!  readonly is  modifiers
-    model: string,
-     year: number
-     } = {
-    type: "Toyota",
-    model: "Corolla",
-    year: 2009
-}
+  readonly type: string; // !n!  readonly is  modifiers
+  model: string;
+  year: number;
+} = {
+  type: "Toyota",
+  model: "Corolla",
+  year: 2009,
+};
 
-car.year = 3000 //? !n! we can modify year cuase it's not readonly type 
+car.year = 3000; //? !n! we can modify year cuase it's not readonly type
 
-car.type = 'bmw' //? !n!  Cannot assign to 'type' because it is a read-only property.
-
-
-
+car.type = "bmw"; //? !n!  Cannot assign to 'type' because it is a read-only property.
