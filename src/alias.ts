b0aae41859daa064745597fa0d16b5c777996186
mdshahjalal5 <muhommodshahjalal@gmail.com>
2024-06-21 15:57:49 +0600

    done

--- src/alias.ts
+++ src/alias.ts
@@ -1,48 +1,50 @@
-// !n! type alias 
-// ! tant => type =>name of type  => type explicit 
+// !n! type alias
+// ! tant => type =>name of type  => type explicit
 
-type multiply = (x:number, y:number) => number;
+type multiply = (x: number, y: number) => number;
 
+// ?imp!  this type function I never used
 
-// ?imp!  this type function I never used 
+const sqr = (
+  n1: number,
+  n2: number,
+  task: (x: number, y: number) => number,
+) => {
+  return task(n1, n2);
+};
 
-const sqr = (n1:number, n2:number, task:(x:number, y:number)=>number) =>  {
-    return task(n1, n2,)
-}
+const res = sqr(3, 5, (x, y) => x * y);
+console.log(res, '"res"');
 
-const res = sqr(3,5, (x, y ) => x * y)
-console.log(res , '"res"')
 type Multiply = (x: number, y: number) => number;
 
 // !n! type alias
- type CarYear = number
-type CarType = string
-type CarModel = string
+type CarYear = number;
+type CarType = string;
+type CarModel = string;
 type Car = {
-  year: CarYear,
-  type: CarType,
-  model: CarModel
-}
+  year: CarYear;
+  type: CarType;
+  model: CarModel;
+};
 
-const carYear: CarYear = 2001
-const carType: CarType = "Toyota"
-const carModel: CarModel = "Corolla"
+const carYear: CarYear = 2001;
+const carType: CarType = "Toyota";
+const carModel: CarModel = "Corolla";
 const car2: Car = {
   year: carYear,
   type: carType,
-  model: carModel
+  model: carModel,
 };
 
 // !n! interfaces
 
 interface Rectangle {
-    height: number, 
-    width: number
+  height: number;
+  width: number;
 }
 
 const rectangle: Rectangle = {
-    height: 20,
-    width: 10
+  height: 20,
+  width: 10,
 };
-
-
--- src/nullishpr.ts
+++ src/nullishpr.ts
@@ -1,41 +1,34 @@
-// !n! nullish coelescing 
+// !n! nullish coelescing
 
-const od:null | undefined | string = '';
-const ev = od??"odvalue" // ?n?  here od's value is not null|undefined so "ev"'s  value not accept "odvalue" 
+const od: null | undefined | string = null;
+const ev = od ?? "odvalue"; // ?n?  here od's value is not null|undefined so "ev"'s  value not accept "odvalue"
 
-// console.log(ev, '"od"');
+console.log(ev, '"od"');
 
-const is_buetian: undefined |boolean |string |null=null
-const nullish = is_buetian?? "yes buetian" 
+const is_buetian: undefined | boolean | string | null = null;
+const nullish = is_buetian ?? "yes bueatian";
 console.log(nullish);
 
-
 function greet(name) {
-    const greeting = `Hello, ${name ?? "World"}!`;
-    console.log(greeting);
+  const greeting = `Hello, ${name ?? "World"}!`;
+  console.log(greeting);
 }
 
 // greet(undefined); // "Hello, World!"
 // greet(null); // "Hello, World!"
 greet("Alice"); // "Hello, Alice!"
 
+type valueType = null | undefined | "" | "ismai";
+let valueType: valueType = "ismai";
 
-type valueType = null | undefined | '' | 'ismai'
-let valueType:valueType = 'ismai'
+// ? 5/28/2023, 8:37:02 AM 5/28/2023, 8:37:03 AM 5/28/2023, 8:37:03 AM 5/28/2023, 8:37:03 AM 5/28/2023, 8:37:03 AM 5/28/2023, 8:37:03 AM
 
-
-
-
-// ? 5/28/2023, 8:37:02 AM 5/28/2023, 8:37:03 AM 5/28/2023, 8:37:03 AM 5/28/2023, 8:37:03 AM 5/28/2023, 8:37:03 AM 5/28/2023, 8:37:03 AM 
-
-
-// ! n! if inputSomething is null | undefined then checkInput value is "no input" 
-const inputSomething:unknown =null;
+// ! n! if inputSomething is null | undefined then checkInput value is "no input"
+const inputSomething: unknown = null;
 const checkInput = inputSomething ?? "No input";
 console.log(checkInput);
 
-
 // !n! if inputSomething2 is not null | undefined then checkInput value will be the inputSomething value
-const inputSomething2: unknown = 'its not null or undefined';
+const inputSomething2: unknown = "its not null or undefined";
 const checkInput2 = inputSomething2 ?? "No input";
-console.log(checkInput2); //?ts not null or undefined'
+console.log(checkInput2); //?ts not null or undefined
